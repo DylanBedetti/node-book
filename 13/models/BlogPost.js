fb7f4439ad2fe8e8e896434efc5bad52535be4f0
Dylan Bedetti <dylanbedetti@gmail.com>
2020-07-26 10:01:31 +0000

    chp 13

---
+++ 13/models/BlogPost.js
@@ -0,0 +1,19 @@
+// Mongoose is a MongoDB object modeling tool designed to work in an asynchronous environment.
+const mongoose = require('mongoose')
+// Creating a Model through a Schema interface
+const Schema = mongoose.Schema;
+
+const BlogPostSchema = new Schema({
+  title: String,
+  body: String,
+  username: String,
+  datePosted: {
+    type: Date,
+    default: new Date()
+  },
+  image: String
+})
+
+const BlogPost = mongoose.model('BlogPost', BlogPostSchema)
+
+module.exports = BlogPost
