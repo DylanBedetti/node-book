5967fe892ee7c2fc676d291690681fe6f32cd8ba
Dylan Bedetti <dylanbedetti@gmail.com>
2020-07-26 06:34:18 +0000

    error control

--- 12/controllers/storeUser.js
+++ 12/controllers/storeUser.js
@@ -5,8 +5,15 @@ module.exports = (req, res) => {
   // console.log(req.body)
   User.create(req.body, (error, user) => {
     if (error) {
+      console.log(error);
+      console.log(error.name)
+
+      // const validationErrors = Object.keys(error.errors).map(key => error.errors[key].message)
+      // const validationErrors = Object.keys(error.errors).map(key => error.errors[key].message)
+
+      // console.log(validationErrors)
+      req.session.validationErrors = error.name;
       return res.redirect('/auth/register');
-      console.log(error, user);
     }
     res.redirect('/');
   });
